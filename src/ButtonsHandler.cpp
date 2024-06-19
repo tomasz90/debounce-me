a9b13595a52073335c420a970447b4008d0147f2
Tomasz Kulig <tomasz.kulig@apzumi.pl>
2024-06-19 20:24:28 +0200

    refactored

--- src/ButtonsHandler.cpp
+++ src/ButtonsHandler.cpp
@@ -1,8 +1,7 @@
 #include "ButtonsHandler.h"
-#include <algorithm>
 
-ButtonsHandler::ButtonsHandler(std::initializer_list<Button> buttons) : buttons(buttons) {
-    std::for_each(this->buttons.begin(), this->buttons.end(), [this](Button &button) {
+ButtonsHandler::ButtonsHandler(std::initializer_list<Button*> buttons) : buttons(buttons) {
+    std::for_each(this->buttons.begin(), this->buttons.end(), [this](Button *button) {
         buttonLastStartPressed[button] = 0;
         buttonWasLongPressed[button] = false;
     });
@@ -16,10 +15,10 @@ void ButtonsHandler::setLongPressTime(unsigned int time) {
     longPressTime = time;
 }
 
-bool ButtonsHandler::pollState(Button &button) const {
-    auto &s = button.state;
+bool ButtonsHandler::pollState(Button *button) const {
+    auto &s = button->state;
     bool stateChanged = false;
-    bool currentRawState = button.invertedState == !digitalRead(button.pin);
+    bool currentRawState = button->invertedState == !digitalRead(button->pin);
     if (s.lastRawState != currentRawState) {
         bool moreThanDebounceTime = (unsigned long) (millis() - s.lastChangeTime) >= debounceTime;
         if (moreThanDebounceTime) {
@@ -32,54 +31,54 @@ bool ButtonsHandler::pollState(Button &button) const {
     return stateChanged;
 }
 
-void ButtonsHandler::resetState(Button &button) const {
-    button.state.lastState = button.state.currentState;
+void ButtonsHandler::resetState(Button *button) const {
+    button->state.lastState = button->state.currentState;
 }
 
-bool ButtonsHandler::isPressed(Button &button) const {
-    return button.state.lastState == PRESSED && button.state.currentState == PRESSED;
+bool ButtonsHandler::isPressed(Button *button) const {
+    return button->state.lastState == PRESSED && button->state.currentState == PRESSED;
 }
 
-bool ButtonsHandler::wasPressed(Button &button) const {
-    return button.state.lastState == PRESSED && button.state.currentState == RELEASED;
+bool ButtonsHandler::wasPressed(Button *button) const {
+    return button->state.lastState == PRESSED && button->state.currentState == RELEASED;
 }
 
-bool ButtonsHandler::wasReleased(Button &button) const {
-    return button.state.lastState == RELEASED && button.state.currentState == PRESSED;
+bool ButtonsHandler::wasReleased(Button *button) const {
+    return button->state.lastState == RELEASED && button->state.currentState == PRESSED;
 }
 
-bool ButtonsHandler::isLongPressed(Button &button) const {
+bool ButtonsHandler::isLongPressed(Button *button) const {
     auto lastStartPressed = buttonLastStartPressed.at(button);
     return millis() - lastStartPressed >= longPressTime && lastStartPressed != 0;
 }
 
-bool& ButtonsHandler::wasLongPressed(Button &button) {
+bool &ButtonsHandler::wasLongPressed(Button *button) {
     return buttonWasLongPressed.at(button);
 }
 
 void ButtonsHandler::poll() {
-    std::for_each(buttons.begin(), buttons.end(), [this](Button &button) {
+    std::for_each(buttons.begin(), buttons.end(), [this](Button *button) {
         pollState(button);
     });
 
-    std::for_each(buttons.begin(), buttons.end(), [this](Button &button) {
+    std::for_each(buttons.begin(), buttons.end(), [this](Button *button) {
         bool isLongPress = isLongPressed(button);
         bool &wasLongPress = wasLongPressed(button);
 
-        if (isPressed(button) && isLongPress && button.isLongPressSupported) {
-            buttonLastStartPressed[button] = button.isMultipleLongPressSupported ? millis() : 0;
+        if (isPressed(button) && isLongPress && button->isLongPressSupported) {
+            buttonLastStartPressed[button] = button->isMultipleLongPressSupported ? millis() : 0;
             wasLongPress = true;
-            button.onPressLong();
-        } else if (wasPressed(button) && !wasLongPress && (!isLongPress || !button.isLongPressSupported)) {
+            button->onPressLong();
+        } else if (wasPressed(button) && !wasLongPress && (!isLongPress || !button->isLongPressSupported)) {
             buttonLastStartPressed[button] = 0;
-            button.onPress();
+            button->onPress();
         } else if (wasReleased(button)) {
             buttonLastStartPressed[button] = millis();
             wasLongPress = false;
         }
     });
 
-    std::for_each(buttons.begin(), buttons.end(), [this](Button &button) {
+    std::for_each(buttons.begin(), buttons.end(), [this](Button *button) {
         resetState(button);
     });
 }
