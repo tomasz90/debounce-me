ec9d3a52bad6c58f700ec1c176b9065c20a0d384
Tomasz Kulig <tomasz.kulig@apzumi.pl>
2024-08-20 21:10:45 +0200

    clean ups

--- src/ButtonsHandler.cpp
+++ src/ButtonsHandler.cpp
@@ -1,6 +1,6 @@
 #include "ButtonsHandler.h"
 
-ButtonsHandler::ButtonsHandler(std::initializer_list<Button*> buttons) : buttons(buttons) {
+ButtonsHandler::ButtonsHandler(std::initializer_list<Button *> buttons) : buttons(buttons) {
     std::for_each(this->buttons.begin(), this->buttons.end(), [this](Button *button) {
         buttonLastStartPressed[button] = 0;
         buttonWasLongPressed[button] = false;
@@ -9,6 +9,14 @@ ButtonsHandler::ButtonsHandler(std::initializer_list<Button*> buttons) : buttons
 
 void ButtonsHandler::setDebounceTime(unsigned int time) {
     debounceTime = time;
+}
+
+void ButtonsHandler::setSimultaneousBehavior(std::set<Button *> _buttons, std::function<void()> behavior) {
+    simultaneousBehaviors[_buttons] = std::move(behavior);
+}
+
+void ButtonsHandler::setSimultaneousBehaviorLong(std::set<Button *> _buttons, std::function<void()> behavior) {
+    simultaneousBehaviorsLong[_buttons] = std::move(behavior);
 }
 
 void ButtonsHandler::pollState(Button *button) const {
@@ -49,6 +57,22 @@ bool &ButtonsHandler::wasLongPressed(Button *button) {
     return buttonWasLongPressed.at(button);
 }
 
+bool ButtonsHandler::isOneButtonPressed() const {
+    return simultaneousButtons.size() < 2;
+}
+
+void ButtonsHandler::simultaneousOnPress() {
+    auto behavior = simultaneousBehaviors[simultaneousButtons];
+    if (behavior) behavior();
+    wasBothPress = true;
+}
+
+void ButtonsHandler::simultaneousOnPressLong() {
+    auto behavior = simultaneousBehaviorsLong[simultaneousButtons];
+    if (behavior) behavior();
+    wasBothPress = true;
+}
+
 void ButtonsHandler::poll() {
     std::for_each(buttons.begin(), buttons.end(), [this](Button *button) {
         pollState(button);
@@ -58,31 +82,28 @@ void ButtonsHandler::poll() {
         bool isLongPress = isLongPressed(button) && button->isLongPressSupported;
         bool &wasLongPress = wasLongPressed(button);
 
+        // LONG PRESS CALLBACKS
         if (isPressed(button) && isLongPress && !wasBothPress) {
-            if(simultaneousButtons.size() < 2) {
+            if (isOneButtonPressed()) {
                 button->onPressLong();
                 wasLongPress = true;
             } else {
-                wasBothPress = true;
-                auto behavior = simultaneousBehaviorsLong[simultaneousButtons];
-                if(behavior) {
-                    behavior();
-                }
+                simultaneousOnPressLong();
             }
             buttonLastStartPressed[button] = button->isMultipleLongPressSupported ? millis() : 0;
             simultaneousButtons.clear();
+
+        // PRESS CALLBACKS
         } else if (wasPressed(button) && !isLongPress && !wasLongPress && !wasBothPress) {
-            if(simultaneousButtons.size() < 2) {
+            if (isOneButtonPressed()) {
                 button->onPress();
             } else {
-                wasBothPress = true;
-                auto behavior = simultaneousBehaviors[simultaneousButtons];
-                if(behavior) {
-                    behavior();
-                }
+                simultaneousOnPress();
             }
             buttonLastStartPressed[button] = 0;
             simultaneousButtons.clear();
+
+        // SET VARIABLES WHEN BUTTON WAS JUST PRESSED
         } else if (wasReleased(button)) {
             buttonLastStartPressed[button] = millis();
             simultaneousButtons.insert(button);
--- src/ButtonsHandler.h
+++ src/ButtonsHandler.h
@@ -12,19 +12,15 @@ class ButtonsHandler {
 public:
     ButtonsHandler(std::initializer_list<Button*> buttons);
 
-    void setSimultaneousBehavior(std::set<Button*> _buttons, std::function<void()> behavior) {
-        simultaneousBehaviors[_buttons] = std::move(behavior);
-    }
-
-    void setSimultaneousBehaviorLong(std::set<Button*> _buttons, std::function<void()> behavior) {
-        simultaneousBehaviorsLong[_buttons] = std::move(behavior);
-    }
-
     void setDebounceTime(unsigned int time);
+    void setSimultaneousBehavior(std::set<Button*> _buttons, std::function<void()> behavior);
+    void setSimultaneousBehaviorLong(std::set<Button*> _buttons, std::function<void()> behavior);
+
     void poll();
 
 private:
     std::vector<Button*> buttons;
+
     std::set<Button*> simultaneousButtons;
     std::map<std::set<Button*>, std::function<void()>> simultaneousBehaviors;
     std::map<std::set<Button*>, std::function<void()>> simultaneousBehaviorsLong;
@@ -37,11 +33,16 @@ private:
 
     void pollState(Button *button) const;
     void resetState(Button *button) const;
+
     bool isPressed(Button *button) const;
     bool wasPressed(Button *button) const;
     bool wasReleased(Button *button) const;
     bool isLongPressed(Button *button) const;
     bool& wasLongPressed(Button *button);
+
+    bool isOneButtonPressed() const;
+    void simultaneousOnPress();
+    void simultaneousOnPressLong();
 };
 
 
